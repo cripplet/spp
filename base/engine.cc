53f62110a4304b6fbc502f4b094b95cae7597069
Minke Zhang <minke.zhang@gmail.com>
2014-05-25 22:42:19 -0500

    Update engine.cc

--- base/engine.cc
+++ base/engine.cc
@@ -8,7 +8,7 @@ Engine::~Engine() {}
 
 void Engine::ignite() { this->status = status_code::INITIALIZED; }
 void Engine::cycle() { this->status = status_code::RUNNING; }
-void Engine::shutdown() { this->status = STOPPED; }
+void Engine::shutdown() { this->status = status_code::STOPPED; }
 
 int Engine::get_status() { return(this->status); }
 int Engine::get_signal() { return(this->signal); }
